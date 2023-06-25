f7834b5ae987a83340888ecf7bb31911396fcf10
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-25 18:35:00 -0400

    Add is_admin field in User model

--- sales/models.py
+++ sales/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractBaseUser
 class User(AbstractBaseUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=128)
+    is_admin = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
