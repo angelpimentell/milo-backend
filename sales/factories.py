18ef612ed9ca5e698deb0d4c98db09ad37a4e8e5
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-25 16:07:58 -0400

    Add sales factories

---
+++ sales/factories.py
@@ -0,0 +1,31 @@
+from hashlib import sha512
+
+import factory
+
+from constants import STRING_ENCODING
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'sales.User'
+
+    name = factory.Faker("name")
+    email = factory.Faker("email")
+    password = sha512("password".encode(STRING_ENCODING)).hexdigest()
+
+
+class CartFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'sales.Cart'
+
+    user = factory.SubFactory(UserFactory)
+    done = factory.Faker("boolean")
+
+
+class InvoiceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'sales.Invoice'
+
+    user = factory.SubFactory(UserFactory)
+    name = factory.Faker("name")
+    paid = factory.Faker("boolean")
