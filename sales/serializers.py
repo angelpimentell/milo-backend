db64b8e0ca513ce73c0381c397ea47596a643e0d
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-25 18:32:40 -0400

    Add fields attributes in serializers

--- sales/serializers.py
+++ sales/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework.serializers import ModelSerializer
 
-from models import User, Cart, Invoice
+from .models import User, Cart, Invoice
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
+        fields = ["id", "name", "email", "created_at", "updated_at"]
 
 
 class CartSerializer(ModelSerializer):
     class Meta:
         model = Cart
+        fields = ["id", "user", "done", "created_at", "updated_at"]
 
 
 class InvoiceSerializer(ModelSerializer):
     class Meta:
         model = Invoice
+        fields = ["id", "user", "name", "paid", "created_at", "updated_at"]
