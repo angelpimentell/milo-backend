e60e819bcc5d686e5a70da5ac565d411a6505345
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-25 16:08:05 -0400

    Add stock factories

---
+++ stock/factories.py
@@ -0,0 +1,30 @@
+import factory
+
+from constants import NUMBER_DECIMALS
+from sales.factories import CartFactory
+
+
+class ProductFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'stock.Product'
+
+    name = factory.Faker("name")
+    quantity = factory.Faker("random_int", min=0, max=15)
+    price = factory.Faker("pydecimal", right_digits=NUMBER_DECIMALS)
+
+
+class ProductCartFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'stock.ProductCart'
+
+    product = factory.SubFactory(ProductFactory)
+    cart = factory.SubFactory(CartFactory)
+
+
+class ProductInvoiceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'stock.ProductInvoice'
+
+    product = factory.SubFactory(ProductFactory)
+    cart = factory.SubFactory(CartFactory)
+    price = factory.Faker("pydecimal", right_digits=NUMBER_DECIMALS)
