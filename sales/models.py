af24bfdec785770ed43c4efa466e8f9d47eab76f
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-25 15:22:16 -0400

    Add models

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        db_table = "users"
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    done = models.BooleanField()
+
+    class Meta:
+        db_table = "carts"
+
+
+class Invoice(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    paid = models.BooleanField()
+
+    class Meta:
+        db_table = "orders"
--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    class Meta:
+        db_table = "products"
+
+
+class ProductCart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey("sales.Cart", on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "products_carts"
+
+
+class ProductInvoice(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey("sales.Invoice", on_delete=models.CASCADE)
+    price = models.FloatField()
+
+    class Meta:
+        db_table = "products_invoices"
