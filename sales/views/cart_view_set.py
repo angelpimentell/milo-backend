4b6a3da315291547f6f73197d2abe2c601e28a64
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-26 20:05:25 -0400

    Add middlewares to CartViewSet

--- sales/views/cart_view_set.py
+++ sales/views/cart_view_set.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 from ..models import Cart
 from ..serializers import CartSerializer
@@ -15,3 +17,5 @@ class CartViewSet(mixins.RetrieveModelMixin,
                   viewsets.ViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
