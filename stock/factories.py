cd913ea2de0b6267ebcf0f8e90c92da41e682852
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-27 18:30:50 -0400

    Add description and image field to Product

--- stock/factories.py
+++ stock/factories.py
@@ -1,6 +1,6 @@
 import factory
 
-from constants import DECIMAL_PLACES
+from constants import DECIMAL_PLACES, MAX_DIGITS
 from sales.factories import CartFactory
 
 
@@ -10,7 +10,9 @@ class ProductFactory(factory.django.DjangoModelFactory):
 
     name = factory.Faker("name")
     quantity = factory.Faker("random_int", min=0, max=15)
-    price = factory.Faker("pydecimal", right_digits=DECIMAL_PLACES)
+    price = factory.Faker("pydecimal", left_digits=MAX_DIGITS, right_digits=DECIMAL_PLACES)
+    description = factory.Faker("text")
+    image = factory.Faker("file_path", category='image')
 
 
 class ProductCartFactory(factory.django.DjangoModelFactory):
--- stock/models.py
+++ stock/models.py
@@ -6,7 +6,9 @@ from constants import DECIMAL_PLACES, MAX_DIGITS
 class Product(models.Model):
     name = models.CharField(max_length=255)
     quantity = models.PositiveIntegerField()
-    price = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    price = models.DecimalField(max_digits=MAX_DIGITS + DECIMAL_PLACES, decimal_places=DECIMAL_PLACES)
+    description = models.TextField(null=True)
+    image = models.ImageField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -27,7 +29,7 @@ class ProductCart(models.Model):
 class ProductInvoice(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey("sales.Invoice", on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=DECIMAL_PLACES)
+    price = models.DecimalField(max_digits=MAX_DIGITS + DECIMAL_PLACES, decimal_places=DECIMAL_PLACES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
