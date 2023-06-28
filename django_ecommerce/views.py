79d33d0e3e787f6bb94fe5f2ddb7081075328704
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-28 19:55:49 -0400

    Change methods used to log in and logout

--- django_ecommerce/views.py
+++ django_ecommerce/views.py
@@ -6,26 +6,25 @@ from django.contrib.auth import authenticate, login, logout
 
 
 class LogInView(APIView):
-    def post(self, request, format=None):
+    authentication_classes = []
+    permission_classes = []
+
+    def post(self, request, **kwargs):
         email = request.data["email"]
         password = request.data["password"]
         user = authenticate(request, username=email, password=password)
 
         if user is not None:
-            login(request, user)
             token = Token.objects.create(user=user)
+
             return Response({"token": token.key}, status=status.HTTP_200_OK)
         else:
             return Response({"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class LogOutView(APIView):
-    def post(self, request, format=None):
-        try:
-            request.user.auth_token.delete()
-        except:
-            pass
 
-        logout(request)
+    def post(self, request, **kwargs):
+        request.user.auth_token.delete()
 
         return Response(status=status.HTTP_204_NO_CONTENT)
