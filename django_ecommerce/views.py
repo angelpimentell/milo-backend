448085f9a119650e7cee162ad940a55018713e36
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-27 20:32:34 -0400

    Add Django login method

--- django_ecommerce/views.py
+++ django_ecommerce/views.py
@@ -1,8 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
 
 
-class LoginView(APIView):
+class LogInView(APIView):
     def post(self, request, format=None):
-        return Response("GOOD")
+        email = request.data["email"]
+        password = request.data["password"]
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response(status=200)
+        else:
+            return Response({"detail": "Invalid credentials."}, status=401)
+
+
+class LogOutView(APIView):
+    def post(self, request, format=None):
+        return Response("BAD")
