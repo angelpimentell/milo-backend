27a52379705b9288646db634f668aed41ccf018d
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-28 14:47:46 -0400

    Add token creation and logout

--- django_ecommerce/views.py
+++ django_ecommerce/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import authenticate, login
-
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate, login, logout
 
 
 class LogInView(APIView):
@@ -13,11 +13,19 @@ class LogInView(APIView):
 
         if user is not None:
             login(request, user)
-            return Response(status=status.HTTP_200_OK)
+            token = Token.objects.create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
         else:
             return Response({"detail": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class LogOutView(APIView):
     def post(self, request, format=None):
-        return Response("BAD")
+        try:
+            request.auth_token.delete()
+        except:
+            pass
+
+        logout(request)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
