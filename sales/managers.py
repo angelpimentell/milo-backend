fef4def52caca73c9d54efa4e274259e2d7b76c0
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-06-30 13:43:54 -0400

    Change comment in managers.py

--- sales/managers.py
+++ sales/managers.py
@@ -5,7 +5,7 @@ from django.contrib.auth.hashers import make_password
 class CustomUserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
         """
-        Create and save a user with the given username, email, and password.
+        Create and save a user with the given email and password.
         """
         email = self.normalize_email(email)
 
