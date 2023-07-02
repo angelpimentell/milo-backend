c5c42a9dd0cd5c6a247a63e9ad1e7f32de7258e4
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-01 20:44:04 -0400

    Change admin field name

--- sales/managers.py
+++ sales/managers.py
@@ -15,13 +15,13 @@ class CustomUserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_admin", False)
+        extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_admin", False)
+        extra_fields.setdefault("is_superuser", False)
 
-        if extra_fields.get("is_admin") is not True:
-            raise ValueError("Superuser must have is_admin=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
         return self._create_user(email, password, **extra_fields)
